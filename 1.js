4cc770df2a4a413ca44764f287c8859f0d44f47e
n-jul <njulkushwaha@gmail.com>
2024-10-23 23:32:10 +0530

    1st question added

---
+++ 1.js
@@ -0,0 +1,37 @@
+const data = 100
+console.log(data.toString());
+console.log(100.toString());
+
+
+
+
+
+
+
+
+/*
+FIRST TRY YOURSELF.
+NOW - 
+The code will give error because we are using . with 100 in to string function and dot replaces means 2 things in js. First is when we access something from an object and 2nd is as decimal.
+So to make it work we can do 2 things.
+1-
+we can wrap 100 in paranthesis
+const data = 100
+console.log(data.toString());
+console.log((100).toString());
+
+2-
+we can add 2 dots 
+const data = 100
+console.log(data.toString());
+console.log(100..toString());
+
+
+
+
+
+
+
+
+
+*/
