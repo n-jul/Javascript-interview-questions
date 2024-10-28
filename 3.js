36b413c5296535df8158a39bd87bf4ed8e3edb48
n-jul <anjulkushwaha11@gmail.com>
2024-10-28 07:48:32 +0530

    3rd question added

---
+++ 3.js
@@ -0,0 +1,21 @@
+console.log(parseInt("108"));
+console.log(parseInt("108*20"));
+console.log(parseInt("108Anjul"));
+
+
+
+
+
+
+
+
+
+
+
+// FIRST TRY YOURSELF
+// NOW - 
+// OUTOUT - 
+// 108
+// 108
+// 108
+// The parseInt method tries everything to convert the parameter into a Integer. So it takes the parameter ans extracts till it finds a valid number and removes other things
